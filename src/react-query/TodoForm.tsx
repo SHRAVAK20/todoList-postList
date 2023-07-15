177bd051b1503541779a54b006fefc2a09da9271
SRK <choudharyshravak@gmail.com>
2023-07-15 10:01:43 +0530

    Infinite query and todo form insert data todolist

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,10 +1,50 @@
-import { useRef } from 'react';
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
+  const queryClient = useQueryClient();
+  const addTodoMutation = useMutation({
+    mutationFn: (todo: Todo) => {
+      return axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data);
+    },
+    onSuccess: (updated_Id_In_Mytodo) => {
+      //Invalidate cache
+
+      // queryClient.invalidateQueries(['todo']);
+
+      //Update Cache
+      console.log(updated_Id_In_Mytodo);
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        updated_Id_In_Mytodo,
+        ...(todos || []),
+      ]);
+    },
+  });
+
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
+    <form
+      className="row mb-3"
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current && ref.current.value)
+          addTodoMutation.mutate({
+            id: 0,
+            title: ref.current.value,
+            userId: 1,
+            completed: true,
+          });
+      }}
+    >
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
